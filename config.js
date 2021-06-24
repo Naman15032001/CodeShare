114e9d1316a15892c2e988e25a88159ed254df3e
Naman15032001 <71309281+Naman15032001@users.noreply.github.com>
2021-06-24 11:15:33 +0530

    Add files via upload

---
+++ config.js
@@ -0,0 +1,15 @@
+'use strict'
+
+module.exports={
+  mailer:{
+    service:'Gmail',
+    auth:{
+      user:'mikejames8690@gmail.com',
+      pass:'naman2001N'
+    }
+
+  },
+  dbConnstring:'mongodb://127.0.0.1:27017/codeshare',
+  sessionKey:'HelloShareCode'
+
+}
